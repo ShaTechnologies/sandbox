752c26f63fd82025d663aee7e789660794637210
ShaTechnologies <shavindramms@live.com>
2016-01-15 12:40:01 +0530

    New Files

--- first_page.php
+++ first_page.php
@@ -7,6 +7,6 @@
     <?php $link_name = "Second Page";?>
     <?php $id = 5;?>
     <?php $company = "Johnson & Johnson"?>
-    <a href="second_page.php?id=<?php echo $id ;?>&company=<?php echo $company; ?>"><?php echo $link_name?></a>
+    <a href="second_page.php?id=<?php echo $id ;?>&company=<?php echo rawurlencode($company); ?>"><?php echo $link_name?></a>
 </body>
 </html>
